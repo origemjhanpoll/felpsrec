61b852eaf3f2f6bb3e481ee38ae14c7215182924
Jean Paul <jean.silva@gupy.com.br>
2023-10-16 01:43:22 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import profileImg from "./assets/images/profile.jpg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href="https://www.instagram.com/felpsfotografia/">
+          <img src={profileImg} className="profile" alt="Felipe Farias" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+      <h2>Felipe Farias</h2>
+      <header>
+        <h3>Desculpe pelo transtorno, estamos em construção!</h3>
+      </header>
+      <section className="description">
+        <p>Caros visitantes,</p>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          Gostaríamos de informar que o nosso site,{" "}
+          <a href="https://felpsrec.com/">https://felpsrec.com/</a>, encontra-se
+          atualmente em construção. Estamos trabalhando arduamente para criar
+          uma experiência online mais aprimorada e repleta de recursos incríveis
+          para atender às suas necessidades.
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <p>
+          Durante esse período de construção, algumas áreas do site podem estar
+          temporariamente indisponíveis. No entanto, estamos fazendo o nosso
+          melhor para garantir que o site esteja de volta o mais rápido possível
+          e melhor do que nunca.
+        </p>
+        <p>
+          Pedimos desculpas por qualquer inconveniência que isso possa causar e
+          agradecemos a sua paciência.
+        </p>
+        <p>
+          Estamos empolgados com as melhorias que estão por vir e mal podemos
+          esperar para compartilhar o novo e aprimorado{" "}
+          <a href="https://felpsrec.com/">https://felpsrec.com/</a> com você em
+          breve. Até lá, agradecemos a sua compreensão e apoio contínuo.
+        </p>
+      </section>
+      <p className="read-the-docs">#origemjhanpoll</p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
